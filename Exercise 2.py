3f68db3b2f625a2725ce2c59b52b5f9e61a07a75
Aditya Sukhwal <33283838+aditya1999s@users.noreply.github.com>
2020-08-05 17:51:36 +0530

    commit

---
+++ Exercise 2.py
@@ -0,0 +1,27 @@
+# Program to perform arithmetic operation menu driven approach
+# Program to check positive number in a list without using if else while
+
+def ar():
+    print("1. Press 1. for addition\n2. Press 2. for subtrction"
+          "\n3. Press 3. for Multiplication\n4. Press 4. for division\nPress 0 to exit.")
+    c = int(input("Enter your choice : "))
+    a = int(input("Enter a : "))
+    b = int(input("Enter b : "))
+    if c == 1:
+        print("Addition of {0} and {1} is : ".format(a,b), a + b)
+    elif c == 2:
+        print("Subtraction of {0} and {1} is : ".format(a,b), a - b)
+    elif c == 3:
+        print("Multiplication of {0} and {1} is : ".format(a, b), a * b)
+    elif c == 4:
+        print("Division of {0} and {1} is : ".format(a, b), a / b)
+    elif c == 0:
+        exit(1)
+    else :
+        print("Invalid choice ")
+
+
+
+
+while True:
+    ar()
