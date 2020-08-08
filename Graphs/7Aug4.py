ccf288abab71010b8481a7226e8833b384433cee
Aditya Sukhwal <33283838+aditya1999s@users.noreply.github.com>
2020-08-08 10:18:06 +0530

    commit

---
+++ Graphs/7Aug4.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(20,10))
+ax1 = fig.add_subplot(121)
+ax2 = fig.add_subplot(122)
+# 1 row 2 column 1 is number of figureas parameter ^
+
+ax1.bar([1,2,3],[3,4,5])
+ax2.bar([1,2,3],[3,4,5])
+
+
+plt.show()
+
+# 7 August exercise
+# create charts with our own value
+# {next week} -> LAMP stack login page with MySQL
+
+# HTML then CSS then {javascript login validation} then PHP { DJANGO if you know } on the backend then MYSql database
